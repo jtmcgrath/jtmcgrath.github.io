8b944973d986530fa3d56e9ab0fb9e8913de4997
jtmcgrath <joe@jmcgrath.co.uk>
2017-01-30 20:48:04 +0200

    Templates & Title Links
    Replaced concatenation-based templates with html5/jQuery solution, and
    expanded external links to entire title to aid usability.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+String.prototype.replaceAll = function(target, replacement) {
+	return this.split(target).join(replacement);
+};
+
 (function( jmPortfolio, $, undefined ) {
 	articleTimeout = '';
-	
+
 	jmPortfolio.hideSplash = function () {
 		setTimeout(function() {$('#splash').fadeOut(400,function() {$('#splash').remove();});}, 500);
 	}
-	
+
 	jmPortfolio.loadDocument = function ( documentId ) {
 		// do nothing if target is current document
 		if (documentId === window.location.pathname.replace(/\//g,'') && !jmPortfolio.loadarticlePopstate && !jmPortfolio.initialLoad) {
 			return;
 		}
-		
+
 		// catch error if target document does not exist
 		if (typeof jmPortfolio.documents[documentId] === 'undefined') {
 			documentId = '404';
 		}
-		
+
 		// if a document is already loaded, hide the current document and set timeout to load target document
 		if (typeof jmPortfolio.documents[window.location.pathname.replace(/\//g,'')] !== 'undefined') {
 			jmPortfolio.hideDocument(false, true);
@@ -25,46 +29,45 @@
 			jmPortfolio.showDocument(documentId);
 		}
 	};
-	
+
 	jmPortfolio.showDocument = function( documentId ) {
 		// get the document via ajax if it has not been preloaded
 		jmPortfolio.getDocument(documentId);
-		
+
 		// manage animation timeout and active class
 		clearTimeout(jmPortfolio.animateTimeout);
 		jmPortfolio.animateTimeout = setTimeout(function() {$('#current-article').css({'overflow-y': 'scroll'})},100);
 		$('.article-list').find('article.active').removeClass('active');
-		
+
 		// define the starting co-ordinates of the showDocument animation
 		if (jmPortfolio.documents[documentId].type === 'project') {
 			var rect = $('#'+documentId)[0].getBoundingClientRect();
 		} else {
 			var rect = {'top': '100%', 'left': 0, 'height': '100%', 'width': '100%'};
-		} 
-		
-		// insert the loading gif if the target document has not been preloaded
-		if (jmPortfolio.documents[documentId].preloaded) {
-			var closeArticle = '</article>';
-		} else {
-			var closeArticle = '<img id="loading" src="/images/loading.gif" alt="" /></article>';
 		}
-		
+
+		// insert the loading gif if the target document has not been preloaded
+		var loading = '<img id="loading" src="/images/loading.gif" alt="" />';
+		if (jmPortfolio.documents[documentId].preloaded) {
+			loading = '';
+		}
+
 		// disable transitions, reposition the document wrapper, and insert the document content
-		$('#current-article').addClass('notransition').css({'top': rect.top, 'left': rect.left, 'height': rect.height, 'width': rect.width}).html('<article>'+jmPortfolio.documents[documentId].content+closeArticle);
-		
+		$('#current-article').addClass('notransition').css({'top': rect.top, 'left': rect.left, 'height': rect.height, 'width': rect.width}).html('<article>'+jmPortfolio.documents[documentId].content+loading+'</article>');
+
 		// force element refresh, then activate transitions
 		$('#current-article').show()[0].offsetHeight;
 		$('#current-article').removeClass('notransition').addClass('active');
 		$('.content-wrapper').addClass('active');
-		
+
 		if (documentId !== '404') {jmPortfolio.updateHistory(documentId);}
-		
+
 		if (jmPortfolio.initialLoad) {
 			jmPortfolio.hideSplash();
 			jmPortfolio.initialLoad = false;
 		}
 	};
-	
+
 	jmPortfolio.hideDocument = function( pushState, freezeControls ) {
 		$('#current-article').css({'overflow-y': 'hidden'}).removeClass('active').stop().animate({scrollTop:0}, '50', 'swing');
 		$('.article-list').find('article.active').removeClass('active');
@@ -73,7 +76,7 @@
 		jmPortfolio.animateTimeout = setTimeout(function() {$('#current-article').hide().html('');}, 500);
 		if (pushState) {window.history.pushState(null, null, '/');}
 	};
-	
+
 	jmPortfolio.getDocument = function( documentId ) {
 		// get the document via ajax if it has not been preloaded
 		if (!jmPortfolio.documents[documentId].preloaded) {
@@ -82,13 +85,13 @@
 				success: function(result) {
 					// set preload to true
 					jmPortfolio.documents[documentId].preloaded = true;
-					
+
 					if (jmPortfolio.documents[documentId].type === 'project') {
 						result = '<section>'+result+'</section>';
 					}
-					
+
 					jmPortfolio.documents[documentId].content = jmPortfolio.documents[documentId].content+result;
-					
+
 					// replace loading icon with result
 					if ($('#loading').length) {
 						$('#loading').replaceWith(result);
@@ -97,12 +100,16 @@
 			});
 		}
 	};
-	
+
 	jmPortfolio.updateHistory = function( documentId ) {
 		if (typeof jmPortfolio.loadarticlePopstate === 'undefined' || jmPortfolio.loadarticlePopstate === false) {
 			window.history.pushState(null, null, documentId);
-			if (jmPortfolio.documents[documentId].type === 'project') {$('#'+documentId).parent('article').addClass('active');}
-		} else {jmPortfolio.loadarticlePopstate = false;}
+			if (jmPortfolio.documents[documentId].type === 'project') {
+				$('#'+documentId).parent('article').addClass('active');
+			}
+		} else {
+			jmPortfolio.loadarticlePopstate = false;
+		}
 	}
 }( window.jmPortfolio = window.jmPortfolio || {}, jQuery ));
 
@@ -111,12 +118,12 @@ $(document).ready(function() {
 		event.preventDefault();
 		jmPortfolio.loadDocument(event.currentTarget.pathname.replace(/\//g,''));
 	});
-	
+
 	$('#current-article').on('click', 'article > span', function(event) {
 		event.preventDefault();
 		jmPortfolio.hideDocument(true, false);
 	});
-	
+
 	$('#controls-close').on('click', function(event) {
 		event.preventDefault();
 		jmPortfolio.hideDocument(true, false);
@@ -132,8 +139,8 @@ $(document).ready(function() {
 			$('.page-header').removeClass('minimise');
 		}
 	});
-	
-	
+
+
 	$.ajax({
 		'async': false,
 		'global': false,
@@ -141,33 +148,53 @@ $(document).ready(function() {
 		'dataType': "json",
 		'success': function (data) {
 			jmPortfolio.documents = data;
-	
-			var jmPortfolioPages = jmPortfolioProjects = '';
-			
+
+			var jmPortfolioPages = jmPortfolioProjects = jmInitialContent = '';
+
 			// Iterate through array
 			$.each(jmPortfolio.documents, function(id, data) {
 				// Construct menu
 				if (data.type === 'page') {
-					jmPortfolioPages += "<li id='" + id + "'><a href=\"" + id + '">' + data.title + "</a></li>";
+					jmPortfolioPages += $('#page-list-item').html()
+						.replaceAll('[id]', id)
+						.replaceAll('[title]', data.title);
 				};
-				
+
 				// Construct project list & project content
 				if (data.type === 'project') {
-					jmPortfolioProjects += '<article id="' + id + '"><a href="/' + id + '"><span><img src="/images/' + id + '.' + data.imagetype + '" height="188px" width="280px" /></span><header><h1>' + data.title + '</h1><h2>' + data.subtitle + '</h2></header></a></article>';
-					
-					var content = '';
-					content += '<article><span><img src="/images/' + id + '.' + data.imagetype + '" height="188px" width="280px" /></span><header><h1>' + data.title;
-					if (typeof data.url !== 'undefined' && data.url !== '') {content += ' <a class="visit-project" href="' + data.url + '" target="_blank">Visit ' + data.title + '</a>';}
-					content += '</h1><h2>' + data.subtitle + '</h2></header></article>';
-					
-					jmPortfolio.documents[id].content = content;
+					// Construct initial content
+				  jmInitialContent = $('#article-initial-content').html()
+						.replaceAll('[id]', id)
+						.replaceAll('[subtitle]', data.subtitle)
+						.replaceAll('[imagetype]', data.imagetype);
+
+					// Add to page list
+				  jmPortfolioProjects += $('#article-list-item').html()
+						.replaceAll('[article-initial-content]', jmInitialContent)
+						.replaceAll('[title]', data.title)
+						.replaceAll('[id]', id)
+						.replace(/(\r\n|\n|\r|  )/gm, '');
+
+					// Construct popup title
+					if (typeof data.url !== 'undefined' && data.url !== '') {
+					  popuptitle = $('#article-popup-link').html()
+							.replaceAll('[url]', data.url)
+							.replaceAll('[title]', data.title);
+					} else {
+						popuptitle = data.title;
+					}
+
+					// Construct popup
+				  jmPortfolio.documents[id].content = $('#article-popup').html()
+						.replaceAll('[article-initial-content]', jmInitialContent)
+						.replaceAll('[title]', popuptitle);
 				};
 			});
-			
+
 			// Insert menu and project list
 			$('.page-header').find('ul').prepend(jmPortfolioPages);
 			$('.article-list').prepend(jmPortfolioProjects);
-			
+
 			// Load page if necessary
 			if (window.location.pathname !== '/') {
 				jmPortfolio.initialLoad = true;
